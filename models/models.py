86f1eb071e742ee85ed875b5ca3f7de77e48acd9
Rafael54x <rafael.natanael46@gmail.com>
2025-11-27 08:50:02 +0700

    Initial commit: project_iht module

---
+++ models/models.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+
+# class project_iht(models.Model):
+#     _name = 'project_iht.project_iht'
+#     _description = 'project_iht.project_iht'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
+
